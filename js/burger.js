364cf4c6a37a66e2993e734e5852c53070d1eb73
makombelajob <50692272+makombelajob@users.noreply.github.com>
2025-03-14 11:44:08 +0100

    burger en place

---
+++ js/burger.js
@@ -0,0 +1,17 @@
+const burger = document.querySelector("#burger");
+const closeBtn = document.querySelector("#close");
+const openList = document.querySelector(".open");
+
+burger.addEventListener("click", function() {
+    openList.style.display = "block";
+});
+
+closeBtn.addEventListener("click", function() {
+    openList.style.display = "none";
+});
+
+document.addEventListener("click", function(e) {
+    if(!burger.contains(e.target) && !closeBtn.contains(e.target) && !openList.contains(e.target)){
+        openList.style.display = "none";
+    }
+});
