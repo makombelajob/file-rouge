9c6b328bb11e6d9139ad2bc891b4202255094140
makombelajob <makombelajob@gmail.com>
2025-06-13 17:02:52 +0200

    reprise de page d'accueil

--- app/src/Controller/HomeController.php
+++ app/src/Controller/HomeController.php
@@ -2,15 +2,19 @@
 
 namespace App\Controller;
 
+use App\Entity\Contacts;
+use App\Form\ContactsForm;
 use App\Repository\CoursesRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(CoursesRepository $coursesRepository): Response
+    public function index(CoursesRepository $coursesRepository, Request $request, EntityManagerInterface $entityManager): Response
     {
         $html = $coursesRepository->findOneBy(['title' => 'HTML']);
         $css = $coursesRepository->findOneBy(['title' => 'CSS']);
@@ -18,6 +22,16 @@ final class HomeController extends AbstractController
         $python = $coursesRepository->findOneBy(['title' => 'PYTHON']);
         $php = $coursesRepository->findOneBy(['title' => 'PHP']);
         $java = $coursesRepository->findOneBy(['title' => 'JAVA']);
-        return $this->render('home/index.html.twig', compact('html', 'css', 'javascript', 'python', 'php', 'java'));
+
+        $contact = new Contacts();
+        $form = $this->createForm(ContactsForm::class, $contact);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($contact);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_home');
+        }
+        return $this->render('home/index.html.twig', compact('html', 'css', 'javascript', 'python', 'php', 'java', 'form'));
     }
 }
