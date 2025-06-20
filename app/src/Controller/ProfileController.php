351deba0b4f4cf579f7a100a218997da0479aa38
makombelajob <makombelajob@gmail.com>
2025-06-20 12:08:27 +0200

    page about done

--- app/src/Controller/ProfileController.php
+++ app/src/Controller/ProfileController.php
@@ -4,17 +4,24 @@ namespace App\Controller;
 
 use App\Repository\CoursesRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 final class ProfileController extends AbstractController
 {
-    #[Route('/profile', name: 'app_profile')]
-    #[IsGranted('ROLE_USER')]
+    #[Route('/profile', name: 'app_profile')]  
     public function index(CoursesRepository $coursesRepository): Response
     {
+        $this->denyAccessUnlessGranted('ROLE_USER');
         $courses = $coursesRepository->findAll();
         return $this->render('profile/index.html.twig', compact('courses'));
+    }
+
+    #[Route('/achat-du-cours', name: 'app_profile_payement')]
+    public function payement():Response
+    {
+        $this->denyAccessUnlessGranted('ROLE_USER');
+        return $this->render('profile/payment.html.twig');
     }
 }
