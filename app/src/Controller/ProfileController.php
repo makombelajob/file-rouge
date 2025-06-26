3d5f7af5dc3f1b4bda326ef5c4c84b5f6a9a60d6
makombelajob <makombelajob@gmail.com>
2025-06-26 11:50:27 +0200

    testing catalogue page

--- app/src/Controller/ProfileController.php
+++ app/src/Controller/ProfileController.php
@@ -2,11 +2,18 @@
 
 namespace App\Controller;
 
+use App\Entity\Courses;
+use App\Entity\Status;
 use App\Repository\CoursesRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Entity\Orders;
+use Stripe\Stripe;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 final class ProfileController extends AbstractController
 {
@@ -18,10 +25,62 @@ final class ProfileController extends AbstractController
         return $this->render('profile/index.html.twig', compact('courses'));
     }
 
-    #[Route('/achat-du-cours', name: 'app_profile_payement')]
-    public function payement():Response
+    #[Route('/achat-du-cours/{id}', name: 'app_payment')]
+    public function payement(EntityManagerInterface $entityManager, Courses $cours):Response
     {
         $this->denyAccessUnlessGranted('ROLE_USER');
-        return $this->render('profile/payment.html.twig');
+
+        // Récupération de l'utilisateur
+        $user = $this->getUser();
+
+        $status = new Status();
+        $status->setName('en cours');
+        $entityManager->persist($status);
+
+        // création de la commande
+        $order = new Orders();
+        $order->setUsers($user);
+        $order->setOrderAt(new \DateTimeImmutable());
+        $order->setOrderTotal($cours->getPrice());
+        $order->setStatus($status);
+        $order->addCourse($cours);
+
+        $entityManager->persist($order);
+        $entityManager->flush();
+        // Introduire le mode paiement
+        Stripe::setApiKey($this->getParameter('stripe.secret.key'));
+
+
+        // creation de la session
+        $session = \Stripe\Checkout\Session::create([
+            'payment_method_types' => ['card'],
+            'line_items' => [[
+                'price_data' => [
+                    'currency' => 'eur',
+                    'unit_amount' => (int) $cours->getPrice() * 100,
+                    'product_data' => [
+                        'name' =>  $cours->getTitle(),
+                    ],
+                ],
+                'quantity' => 1,
+            ]],
+            'mode' => 'payment',
+            'success_url' => $this->generateUrl('payment_success', [], UrlGeneratorInterface::ABSOLUTE_URL),
+            'cancel_url' => $this->generateUrl('payment_cancel', [], UrlGeneratorInterface::ABSOLUTE_URL),
+        ]);
+        // fin de la session
+        return new RedirectResponse($session->url);
+    }
+
+    #[Route('/payment/success', name: 'payment_success')]
+    public function success(): Response
+    {
+        return $this->render('profile/success.html.twig');
+    }
+
+    #[Route('/payment/cancel', name: 'payment_cancel')]
+    public function cancel(): Response
+    {
+        return $this->render('profile/cancel.html.twig');
     }
 }
